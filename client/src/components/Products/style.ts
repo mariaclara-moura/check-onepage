5b7c45baa54520310cc45e344e638bd080bb727c
Maria Clara Moura <mcam3@cin.ufpe.br>
2025-01-27 20:03:05 -0300

    Update style.ts

--- client/src/components/Products/style.ts
+++ client/src/components/Products/style.ts
@@ -119,7 +119,6 @@ export const ButtonGroup = styled.div`
     }
     @media (min-width: 1500px) {
         gap: 2.5rem;
-        width: 88%;
         }
     //885-760-570
     @media (max-width: 500px) {
